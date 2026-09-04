08e728d54b010715ef54c3692413ceb553fe746b
wangmingjie36-creator <wangmingjie36@gmail.com>
2026-09-04 15:03:55 -0700

    Alpha Hive 蜂群日报 2026-09-04 15:03

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Alpha Hive Service Worker - 20260903-1455
-var CACHE_NAME='alpha-hive-20260903-1455';
+// Alpha Hive Service Worker - 20260904-1503
+var CACHE_NAME='alpha-hive-20260904-1503';
 var PRECACHE_URLS=['./', 'index.html', 'manifest.json',
   'chart.umd.min.js'];  // v0.41.0: Chart.js 自托管（jsdelivr 大陆不可达）
 
